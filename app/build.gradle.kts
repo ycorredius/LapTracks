97c5b062cbbadaa5df3ef699fd11283d85febbc1
Shaun Yarbrough <ycorredius@gmail.com>
2024-01-13 08:29:24 -0500

    Fixes build.gradle files for hilt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
-  id("com.google.devtools.ksp") version "1.9.22-1.0.16"
-  id("kotlin-kapt")
-  id("dagger.hilt.android.plugin")
+  kotlin("kapt")
+  id("com.google.devtools.ksp")
+  id("com.google.dagger.hilt.android")
 }
 
 android {
