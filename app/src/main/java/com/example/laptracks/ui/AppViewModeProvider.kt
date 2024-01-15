46a8f4b356479a0db9aecb70b83f690b76f7541a
Shaun Yarbrough <ycorredius@gmail.com>
2024-01-15 09:17:42 -0500

    Removed unnecessary workoutviewmodel parameter.

--- app/src/main/java/com/example/laptracks/ui/AppViewModeProvider.kt
+++ app/src/main/java/com/example/laptracks/ui/AppViewModeProvider.kt
@@ -1,19 +1,17 @@
 package com.example.laptracks.ui
 
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.laptracks.LapTrackApplication
-import com.example.laptracks.ui.viewmodels.WorkoutViewModel
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
-import androidx.lifecycle.createSavedStateHandle
 import com.example.laptracks.ui.viewmodels.StudentEntryViewModel
+import com.example.laptracks.ui.viewmodels.WorkoutViewModel
 
 object AppViewModelProvider {
   val Factory = viewModelFactory {
    initializer {
       WorkoutViewModel(
-        this.createSavedStateHandle(),
         lapTrackApplication().container.studentRepository)
    }
 
