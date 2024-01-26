9590d96fab81dfe4c3b97facdeddcd7d38f3c743
Shaun Yarbrough <ycorredius@gmail.com>
2024-01-26 16:07:55 -0500

    Adds hilt variables for build gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
   val nav_version = "2.7.6"
+  val hilt_version = "2.50"
 
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -66,8 +67,8 @@ dependencies {
   ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
   implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 
-  implementation("com.google.dagger:hilt-android:2.50")
-  kapt("com.google.dagger:hilt-android-compiler:2.50")
+  implementation("com.google.dagger:hilt-android:$hilt_version")
+  kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
   implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
   kapt("androidx.hilt:hilt-compiler:1.1.0")
 
