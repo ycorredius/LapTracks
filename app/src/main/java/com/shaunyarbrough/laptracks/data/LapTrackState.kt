7e16ede08497772eb92f8c7202fabe954703fb44
Shaun Yarbrough <ycorredius@gmail.com>
2024-02-08 11:12:18 -0500

    Removes unnecessary files.

--- app/src/main/java/com/shaunyarbrough/laptracks/data/LapTrackState.kt
+++
@@ -1,7 +0,0 @@
-package com.shaunyarbrough.laptracks.data
-
-data class LapTrackState(
-  val participants: List<String> = listOf(),
-  val interval: String = "Intervals",
-  val participantTimes: Map<String, List<Long>> = mapOf()
-)
