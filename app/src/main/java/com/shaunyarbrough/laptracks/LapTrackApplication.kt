591ce0f582fbe1e91071301811a41cc06feba21a
Shaun Yarbrough <ycorredius@gmail.com>
2024-02-15 23:06:13 -0500

    This is all manual injection not needed in the project because of hilt. Will remove soon.

--- app/src/main/java/com/shaunyarbrough/laptracks/LapTrackApplication.kt
+++ app/src/main/java/com/shaunyarbrough/laptracks/LapTrackApplication.kt
@@ -1,20 +1,9 @@
 package com.shaunyarbrough.laptracks
 
 import android.app.Application
-import com.shaunyarbrough.laptracks.data.AppContainer
-import com.shaunyarbrough.laptracks.data.AppDataContainer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp //If I add this is breaks. The Error message is not very descriptive
 class LapTrackApplication : Application() {
 
-  /**
-   * AppContainer instance used by the rest of classes to obtain dependencies
-   */
-  lateinit var container: AppContainer
-
-  override fun onCreate() {
-    super.onCreate()
-    container = AppDataContainer(this)
-  }
 }
